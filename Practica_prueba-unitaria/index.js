07c07e79a5387e138422b5645715c4bf7187116d
Ahs <92954050+AhslexR@users.noreply.github.com>
2023-12-16 21:09:08 -0400

    Delete Practica_prueba-unitaria directory

--- Practica_prueba-unitaria/index.js
+++
@@ -1,8 +0,0 @@
-// hola-mundo.js
-
-function holaMundo() {
-    console.log("¡Hola, Mundo!");
-  }
-  
-  module.exports = holaMundo; // Exporta la función para que pueda ser importada en las pruebas
-  
